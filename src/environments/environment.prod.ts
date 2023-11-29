09072c67b188bc7c6c28d3b87ffd2d0fd0c12819
ElMismisimo <joi.saez@duocuc.cl>
2023-11-29 06:38:14 -0300

    WE WIN THOSE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase} from 'firebase/database';
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDDJmQN-xZgdTnFDSnlGVxq7E9YDgJ3ahQ",
+    authDomain: "prototipo2-c4926.firebaseapp.com",
+    databaseURL: "https://prototipo2-c4926-default-rtdb.firebaseio.com",
+    projectId: "prototipo2-c4926",
+    storageBucket: "prototipo2-c4926.appspot.com",
+    messagingSenderId: "248909440083",
+    appId: "1:248909440083:web:ac7c1b50a7867d83f2d2a6",
+    measurementId: "G-FKWMTNWVX2"
+  },
+  
 };
+export const firebaseApp =  initializeApp(environment.firebaseConfig);
+export const database = getDatabase(firebaseApp);
